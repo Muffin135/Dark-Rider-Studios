698368984c707c70396b6bfb16ac97ad488b977a
Muffin135 <lionelqwork@gmail.com>
2017-12-08 15:37:48 -0800

    Update darkrider.js

--- darkrider.js
+++ darkrider.js
@@ -3,19 +3,9 @@ function changeContent(elem){
 	document.getElementById("image-one").classList.toggle("change");
 	document.getElementById("tab-one").classList.toggle("change");
 	document.getElementById("tix-begin").classList.toggle("add");
-	document.getElementById("proceed-two").classList.remove("fortix");
-	document.getElementById("proceed-two").classList.remove("fortix:hover");
-	document.getElementById("payup-two").innerHTML = "$__";
-}
-
-function changeContentTwo(elem){
-	document.getElementById("body-two").classList.toggle("buy");
-	document.getElementById("image-two").classList.toggle("change");
-	document.getElementById("tab-two").classList.toggle("change");
-	document.getElementById("tix-begin-two").classList.toggle("add");
-	document.getElementById("proceed-two").classList.remove("fortix");
-	document.getElementById("proceed-two").classList.remove("fortix:hover");
-	document.getElementById("payup-two").innerHTML = "$__";
+	document.getElementById("proceed").classList.remove("fortix");
+	document.getElementById("proceed").classList.remove("fortix:hover");
+	document.getElementById("payup").innerHTML = "$__";
 }
 
 function addTix1(elem){
@@ -60,42 +50,6 @@ function addTix7(elem){
 	document.getElementById("payup").innerHTML = "$50";
 }
 
-function addTix14(elem){
-	document.getElementById("proceed-two").classList.add("fortix");
-	document.getElementById("proceed-two").classList.add("fortix:hover");
-	document.getElementById("payup-two").innerHTML = "$50";
+function playHoeing(e) {
+	document.getElementById("hoeing").play();
 }
-
-function addTix15(elem){
-	document.getElementById("proceed-two").classList.add("fortix");
-	document.getElementById("proceed-two").classList.add("fortix:hover");
-	document.getElementById("payup-two").innerHTML = "$50";
-}
-
-function addTix16(elem){
-	document.getElementById("proceed-two").classList.add("fortix");
-	document.getElementById("proceed-two").classList.add("fortix:hover");
-	document.getElementById("payup-two").innerHTML = "$50";
-}
-
-function addTix17(elem){
-	document.getElementById("proceed-two").classList.add("fortix");
-	document.getElementById("proceed-two").classList.add("fortix:hover");
-	document.getElementById("payup-two").innerHTML = "$50";
-}
-
-function addTix18(elem){
-	document.getElementById("proceed-two").classList.add("fortix");
-	document.getElementById("proceed-two").classList.add("fortix:hover");
-	document.getElementById("payup-two").innerHTML = "$50";
-}
-
-
-
-
-
-
-
-
-
-
